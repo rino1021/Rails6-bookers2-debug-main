db78c8371643944a31a8271a47ef04a5702fa6ab
EC2 Default User <ec2-user@ip-172-31-42-106.ec2.internal>
2022-06-22 13:16:50 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
       render 'index'
@@ -43,6 +43,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title,:body)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
+　has_many :books, dependent: :destroy
   has_one_attached :profile_image
-  has_many :books
+
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
 
