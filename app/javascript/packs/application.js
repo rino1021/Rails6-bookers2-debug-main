859352f1d0c049bea8ed51fb9300aa5ace1935ec
EC2 Default User <krat1021007@gmail.com>
2022-07-09 07:32:05 +0000

    Final

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
-import "popper.js"
+import "script.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application"
